6fbfed4601d2a4ee3138fae8f147795faced6e6f
Lizard -v <swooosy@gmail.com>
2022-06-21 16:30:33 +0900

    Main 최종 완성 / Contact 수정 요청

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// 스크롤시 NAVBAR 배경
+$(window).scroll(function() {
+  const scroll = $(window).scrollTop();
+  if (scroll >= 50) {
+    $("header").addClass("navbg");
+  } else {
+      $("header").removeClass("navbg");
+    }
+});
+
+// 반응형 일때 사이드 메뉴
+$('.show-btn').click(function(){
+  $('.nav-m').fadeIn()
+})
+$('.hide-btn').click(function(){
+  $('.nav-m').fadeOut()
+})
+
+// AOS JS
+AOS.init();
+
+// POPUP 버튼
+$('.pop-show').click(function(){
+  $('.modal').fadeIn()
+})
+$('.pop-hide').click(function(){
+  $('.modal').fadeOut()
+})
